0277ba1f59eb300d2d2158c72d8644f743d637e5
Chaitanya Sanivada <saichaitanyas@voltmatrix.net>
2025-04-09 16:11:36 +0530

    fix spacing in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,8 @@ import CurrentTime from "./CurrentTime";
 import ClientLogo from "./ClientLogo";
 const styles = {
   header: {
-    // width: "fit-content",
-
     width: "100%",
-    textAlign: "center",
-
-    // top: "0px",
-    // left: "calc(50vw + 80px)", // position left edge at 50vw + (width of side bar)/2
-    // left: "0px",
-    // transform: "translateX(-50%)",
     zIndex: 1000,
-    padding: "9px 20px 9px 20px",
     boxShadow: "0px 2px 2px rgba(0,0,0,0.25)",
     borderRadius: "3px",
     backgroundColor: "#006400", // "#196f5d", /* "#0077b6",*/ //"#0096c7",
